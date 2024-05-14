8a67b404ea3353458ea1f55e2c3c6e5c301ab491
zhuliquan <zlqlovecode@foxmail.com>
2024-05-15 00:36:29 +0800

    feat: wilrdcard and regexp

--- lucene_to_sql.go
+++ lucene_to_sql.go
@@ -1,12 +1,26 @@
 package lucene_to_sql
 
 import (
+	"errors"
 	"fmt"
+	"strings"
+
 	// "strings"
 
-	"github.com/zhuliquan/lucene_parser"
-	// "github.com/zhuliquan/lucene_parser/term"
 	esMapping "github.com/zhuliquan/es-mapping"
+	"github.com/zhuliquan/lucene_parser"
+	"github.com/zhuliquan/lucene_parser/term"
+)
+
+type SQL_STYLE int32
+
+const (
+	Standard SQL_STYLE = iota // SQL99
+	SQLLite
+	MySQL
+	Oracle
+	PostgreSQL
+	ClickHouse
 )
 
 type Tokenizer interface {
@@ -17,7 +31,9 @@ type SqlConvertor struct {
 	// field => tokenizer
 	tokenizers map[string]Tokenizer
 
-	mappings *esMapping.Mapping
+	mappings *esMapping.PropertyMapping
+
+	sqlStyle SQL_STYLE
 }
 
 func WithTokenizer(field string, tokenizer Tokenizer) func(s *SqlConvertor) {
@@ -26,9 +42,10 @@ func WithTokenizer(field string, tokenizer Tokenizer) func(s *SqlConvertor) {
 	}
 }
 
-func WithSchema(mappings *esMapping.Mapping) func(s *SqlConvertor) {
+func WithSchema(mappings *esMapping.PropertyMapping) func(s *SqlConvertor) {
 	return func(s *SqlConvertor) {
-		s.mappings = mappings
+		s.mappings = &esMapping.PropertyMapping{}
+
 	}
 }
 
@@ -40,28 +57,28 @@ func NewSqlConvertor(options ...func(s *SqlConvertor)) *SqlConvertor {
 	return s
 }
 
-func LuceneToSql(query string) (string, error) {
+func (c *SqlConvertor) LuceneToSql(query string) (string, error) {
 	lucene, err := lucene_parser.ParseLucene(query)
 	if err != nil {
 		return "", err
 	}
-	return luceneToSql(lucene)
+	return c.luceneToSql(lucene)
 }
 
-func luceneToSql(lucene *lucene_parser.Lucene) (string, error) {
+func (c *SqlConvertor) luceneToSql(lucene *lucene_parser.Lucene) (string, error) {
 	sql := NewSQL()
-	str, err := orQueryToSql(lucene.OrQuery)
+	str, err := c.orQueryToSql(lucene.OrQuery)
 	if err != nil {
-		return "", fmt.Errorf("failed to convert OR clause, err: %+v", err)
+		return "", fmt.Errorf("failed to convert OR clause, err: %w", err)
 	}
 	err = sql.AddORClause(str, false)
 	if err != nil {
 		return "", err
 	}
 	for _, subQuery := range lucene.OSQuery {
-		str, err = orQueryToSql(subQuery.OrQuery)
+		str, err = c.orQueryToSql(subQuery.OrQuery)
 		if err != nil {
-			return "", fmt.Errorf("failed to convert OR clause, err: %+v", err)
+			return "", fmt.Errorf("failed to convert OR clause, err: %w", err)
 		}
 		err = sql.AddORClause(str, true)
 		if err != nil {
@@ -71,11 +88,11 @@ func luceneToSql(lucene *lucene_parser.Lucene) (string, error) {
 	return sql.String(), nil
 }
 
-func orQueryToSql(orQuery *lucene_parser.OrQuery) (string, error) {
+func (c *SqlConvertor) orQueryToSql(orQuery *lucene_parser.OrQuery) (string, error) {
 	sql := NewSQL()
-	str, err := andQueryToSql(orQuery.AndQuery)
+	str, err := c.andQueryToSql(orQuery.AndQuery)
 	if err != nil {
-		return "", fmt.Errorf("failed to convert AND clause, err: %+v", err)
+		return "", fmt.Errorf("failed to convert AND clause, err: %w", err)
 	}
 	err = sql.AddAndClause(str, false, false)
 	if err != nil {
@@ -86,9 +103,9 @@ func orQueryToSql(orQuery *lucene_parser.OrQuery) (string, error) {
 		if subQuery.NotSymbol != nil {
 			reverse = true
 		}
-		str, err = andQueryToSql(subQuery.AndQuery)
+		str, err = c.andQueryToSql(subQuery.AndQuery)
 		if err != nil {
-			return "", fmt.Errorf("failed to convert AND clause, err: %+v", err)
+			return "", fmt.Errorf("failed to convert AND clause, err: %w", err)
 		}
 		err = sql.AddAndClause(str, true, reverse)
 		if err != nil {
@@ -98,54 +115,132 @@ func orQueryToSql(orQuery *lucene_parser.OrQuery) (string, error) {
 	return sql.String(), nil
 }
 
-func andQueryToSql(andQuery *lucene_parser.AndQuery) (string, error) {
+func (c *SqlConvertor) andQueryToSql(andQuery *lucene_parser.AndQuery) (string, error) {
 	reverse := false
 	if andQuery.NotSymbol != nil {
 		reverse = true
 	}
 	if andQuery.ParenQuery != nil {
 		sql := NewSQL()
-		str, err := parenToSql(andQuery.ParenQuery)
+		str, err := c.parenToSql(andQuery.ParenQuery)
 		if err != nil {
 			return "", err
 		}
 		err = sql.AddSubClause(str, reverse)
 		if err != nil {
-			return "", fmt.Errorf("failed to convert Sub Lucene, err: %+v", err)
+			return "", fmt.Errorf("failed to convert Sub Lucene, err: %w", err)
 		}
 		return sql.String(), nil
 	} else {
-		return fieldToSql(andQuery.FieldQuery, reverse)
+		return c.termQueryToSql(andQuery.FieldQuery, reverse)
 	}
 }
 
-func parenToSql(parenQuery *lucene_parser.ParenQuery) (string, error) {
-	str, err := luceneToSql(parenQuery.SubQuery)
+func (c *SqlConvertor) parenToSql(parenQuery *lucene_parser.ParenQuery) (string, error) {
+	str, err := c.luceneToSql(parenQuery.SubQuery)
 	if err != nil {
 		return "", err
 	}
 	return fmt.Sprintf("( %s )", str), nil
 }
 
-func fieldToSql(fieldQuery *lucene_parser.FieldQuery, reverse bool) (string, error) {
-	// field := fieldQuery.Field.String()
-	// value := fieldQuery.Term
-	// termType := value.GetTermType()
-	// if termType & term.SINGLE_TERM_TYPE == term.SINGLE_TERM_TYPE {
-	// 	return "", nil
-	// }
-	// if termType & term.PHRASE_TERM_TYPE == term.PHRASE_TERM_TYPE {
-	// 	return "", nil
-	// }
-	// if termType & term.REGEXP_TERM_TYPE == term.REGEXP_TERM_TYPE {
-	// 	return "", nil
-	// }
-	// if termType & term.TermType(term.BOOLEAN_FIELD_TYPE) {
-	// 	return "", nil
-	// }
+func (c *SqlConvertor) termQueryToSql(termQuery *lucene_parser.FieldQuery, reverse bool) (string, error) {
+	field := termQuery.Field.String()
+	if field == "" {
+		return "", errors.New("field is empty")
+	}
+	value := termQuery.Term
+	typMap, tErr := c.mappings.GetProperty(field)
+	if tErr != nil || len(typMap) == 0 {
+		return "", fmt.Errorf("failed to get field: %s property, err: %v", field, tErr)
+	}
+	tType := typMap[field]
+	var sql string
+	var err error
+	switch {
+	case value.GetTermType()&term.REGEXP_TERM_TYPE == term.REGEXP_TERM_TYPE:
+		sql, err = c.regexpQueryToSql(field, tType, termQuery.Term)
+	case value.GetTermType()&term.RANGE_TERM_TYPE == term.RANGE_TERM_TYPE:
+	case value.GetTermType()&term.WILDCARD_TERM_TYPE == term.WILDCARD_TERM_TYPE:
+		sql, err = c.wildcardQueryToSql(field, tType, termQuery.Term)
+	case value.GetTermType()&term.SINGLE_TERM_TYPE == term.SINGLE_TERM_TYPE:
+		//
+	case value.GetTermType()&term.PHRASE_TERM_TYPE == term.PHRASE_TERM_TYPE:
+		//
 
-	// switch value.GetTermType() {
+	case value.GetTermType()&term.FUZZY_TERM_TYPE == term.FUZZY_TERM_TYPE:
+		//
+	}
+	if err != nil {
+		return "", err
+	}
+	if reverse {
+		return fmt.Sprintf("NOT (%s)", sql), nil
+	}
+	return sql, nil
+}
 
-	// }
+func (c *SqlConvertor) rangeQueryToSql(field string, value *term.Term) (string, error) {
+	sql := NewSQL()
+	bnd := value.GetBound()
+	lVal := bnd.LeftValue
+	// rVal := bnd.RightValue
+
+	if !lVal.IsInf(0) {
+		if bnd.LeftInclude {
+			sql.AddAndClause(fmt.Sprintf("%s >= %s", field), true, false)
+		} else {
+			sql.AddAndClause(fmt.Sprintf("%s > %s", field), true, false)
+		}
+	}
 	return "", nil
+
+}
+
+func (c *SqlConvertor) regexpQueryToSql(
+	field string, tType *esMapping.Property, value *term.Term,
+) (string, error) {
+	if esMapping.CheckStringType(tType.Type) {
+		switch c.sqlStyle {
+		case SQLLite, MySQL:
+			return fmt.Sprintf("%s REGEXP %q", field, value.String()), nil
+		case Oracle:
+			return fmt.Sprintf("regexp_like(%s, %q)", field, value.String()), nil
+		case ClickHouse:
+			return fmt.Sprintf("match(%s, %q)", field, value.String()), nil
+		default:
+			// sql99 and postgresql
+			return fmt.Sprintf("%s SIMILAR TO %q", field, value.String()), nil
+		}
+	} else {
+		return "", fmt.Errorf("expect field: %s string type, but: %s", field, tType.Type)
+	}
+}
+
+func (c *SqlConvertor) wildcardQueryToSql(
+	field string, tType *esMapping.Property, value *term.Term,
+) (string, error) {
+	if esMapping.CheckStringType(tType.Type) {
+		switch c.sqlStyle {
+		case SQLLite:
+			return fmt.Sprintf("%s GLOB %q", field, value.String()), nil
+		default:
+			tks := []string{value.FuzzyTerm.SingleTerm.Begin}
+			tks = append(tks, value.FuzzyTerm.SingleTerm.Chars...)
+			pattern := []string{}
+			for _, tk := range tks {
+				switch tk {
+				case "?":
+					pattern = append(pattern, "_")
+				case "*":
+					pattern = append(pattern, "%")
+				default:
+					pattern = append(pattern, tk)
+				}
+			}
+			return fmt.Sprintf("%s LIKE %q", field, strings.Join(pattern, "")), nil
+		}
+	} else {
+		return "", fmt.Errorf("expect field: %s string type, but: %s", field, tType.Type)
+	}
 }
