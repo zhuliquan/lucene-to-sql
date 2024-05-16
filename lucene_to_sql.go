84ba4b16283a83705e9951fdc3637c8e33e500af
zhuliquan <zlqlovecode@foxmail.com>
2024-05-17 00:15:07 +0800

    feat: range query to sql

--- lucene_to_sql.go
+++ lucene_to_sql.go
@@ -16,12 +16,29 @@ type SQL_STYLE int32
 
 const (
 	Standard SQL_STYLE = iota // SQL99
-	SQLLite
+	SQLite
 	MySQL
 	Oracle
 	PostgreSQL
 	ClickHouse
 )
+
+func (s SQL_STYLE) String() string {
+	switch s {
+	case SQLite:
+		return "SQLite"
+	case MySQL:
+		return "MySQL"
+	case Oracle:
+		return "Oracle"
+	case PostgreSQL:
+		return "PostgreSQL"
+	case ClickHouse:
+		return "ClickHouse"
+	default:
+		return "SQL99"
+	}
+}
 
 type Tokenizer interface {
 	Split(string) []string
@@ -165,13 +182,14 @@ func (c *SqlConvertor) termQueryToSql(termQuery *lucene_parser.FieldQuery, rever
 	case value.GetTermType()&term.WILDCARD_TERM_TYPE == term.WILDCARD_TERM_TYPE:
 		sql, err = c.wildcardQueryToSql(field, tType, termQuery.Term)
 	case value.GetTermType()&term.SINGLE_TERM_TYPE == term.SINGLE_TERM_TYPE:
-		//
+		sql, err = c.singleQueryToSql(field, tType, termQuery.Term)
 	case value.GetTermType()&term.PHRASE_TERM_TYPE == term.PHRASE_TERM_TYPE:
-		//
+		sql, err = c.phraseQueryToSql(field, tType, termQuery.Term)
 	case value.GetTermType()&term.FUZZY_TERM_TYPE == term.FUZZY_TERM_TYPE:
-		err = errors.New("not support fuzzy query")
+		sql, err = c.fuzzyQueryToSql(field, tType, termQuery.Term)
 	case value.GetTermType()&term.GROUP_TERM_TYPE == term.GROUP_TERM_TYPE:
-		//
+		lucene := lucene_parser.TermGroupToLucene(termQuery.Field, value.TermGroup)
+		sql, err = c.luceneToSql(lucene)
 	}
 	if err != nil {
 		return "", err
@@ -180,6 +198,18 @@ func (c *SqlConvertor) termQueryToSql(termQuery *lucene_parser.FieldQuery, rever
 		return fmt.Sprintf("NOT (%s)", sql), nil
 	}
 	return sql, nil
+}
+
+func (c *SqlConvertor) singleQueryToSql(
+	field string, _ *esMapping.Property, value *term.Term,
+) (string, error) {
+	return "", nil
+}
+
+func (c *SqlConvertor) phraseQueryToSql(
+	field string, _ *esMapping.Property, value *term.Term,
+) (string, error) {
+	return "", nil
 }
 
 func (c *SqlConvertor) rangeQueryToSql(
@@ -213,7 +243,7 @@ func (c *SqlConvertor) regexpQueryToSql(
 ) (string, error) {
 	if esMapping.CheckStringType(tType.Type) {
 		switch c.sqlStyle {
-		case SQLLite, MySQL:
+		case SQLite, MySQL:
 			return fmt.Sprintf("%s REGEXP %q", field, value.String()), nil
 		case Oracle:
 			return fmt.Sprintf("regexp_like(%s, %q)", field, value.String()), nil
@@ -233,7 +263,7 @@ func (c *SqlConvertor) wildcardQueryToSql(
 ) (string, error) {
 	if esMapping.CheckStringType(tType.Type) {
 		switch c.sqlStyle {
-		case SQLLite:
+		case SQLite:
 			return fmt.Sprintf("%s GLOB %q", field, value.String()), nil
 		default:
 			tks := []string{value.FuzzyTerm.SingleTerm.Begin}
@@ -250,6 +280,28 @@ func (c *SqlConvertor) wildcardQueryToSql(
 				}
 			}
 			return fmt.Sprintf("%s LIKE %q", field, strings.Join(pattern, "")), nil
+		}
+	} else {
+		return "", fmt.Errorf("expect field: %s string type, but: %s", field, tType.Type)
+	}
+}
+
+func (c *SqlConvertor) fuzzyQueryToSql(
+	field string, tType *esMapping.Property, value *term.Term,
+) (string, error) {
+	if esMapping.CheckStringType(tType.Type) {
+		switch c.sqlStyle {
+		case SQLite:
+			//
+			return "", nil
+		case PostgreSQL:
+			//
+			return "", nil
+		case ClickHouse:
+			//
+			return "", nil
+		default:
+			return "", fmt.Errorf("%s is not support fuzzy query", c.sqlStyle)
 		}
 	} else {
 		return "", fmt.Errorf("expect field: %s string type, but: %s", field, tType.Type)
