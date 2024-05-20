cd89a05306d165900bb4da5c0b14fd7ae9c3a56d
zhuliquan <zlqlovecode@foxmail.com>
2024-05-20 23:21:46 +0800

    test: test cases for range query to sql

--- lucene_to_sql_test.go
+++ lucene_to_sql_test.go
@@ -21,10 +21,121 @@ func TestLuceneToSQL(t *testing.T) {
 			name: "test range left value error number",
 			opts: []func(*SqlConvertor){
 				WithSQLStyle(SQLite),
-				WithSchema(&esMapping.PropertyMapping{}),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.INTEGER_FIELD_TYPE,
+						},
+					},
+				})),
 			},
-			query: "",
-			wantSQL: "",
+			query:   "field:[\"x\" TO *]",
+			wantErr: true,
+		},
+		{
+			name: "test range left value string value and include",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.TEXT_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:[87'yu TO *]",
+			wantSQL: "field >= '87''yu'",
+		},
+		{
+			name: "test range left phrase value string value and include",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.TEXT_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:[\"87'yu\" TO *]",
+			wantSQL: "field >= '87''yu'",
+		},
+		{
+			name: "test range left value number value and exclude",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.INTEGER_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:{87 TO *]",
+			wantSQL: "field > 87",
+		},
+		{
+			name: "test range right value error number",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.INTEGER_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:[87 TO \"x\"]",
+			wantErr: true,
+		},
+		{
+			name: "test range right value string value and include",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.TEXT_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:[* TO 87yu]",
+			wantSQL: "field <= '87yu'",
+		},
+		{
+			name: "test range right phrase value string value and include",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.TEXT_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:[* TO \"87yu\"]",
+			wantSQL: "field <= '87yu'",
+		},
+		{
+			name: "test range right value number value and exclude",
+			opts: []func(*SqlConvertor){
+				WithSQLStyle(SQLite),
+				WithSchema(getSchema(&esMapping.Mapping{
+					Properties: map[string]*esMapping.Property{
+						"field": {
+							Type: esMapping.INTEGER_FIELD_TYPE,
+						},
+					},
+				})),
+			},
+			query:   "field:[* TO 87}",
+			wantSQL: "field < 87",
 		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
@@ -37,4 +148,9 @@ func TestLuceneToSQL(t *testing.T) {
 			}
 		})
 	}
+}
+
+func getSchema(mapping *esMapping.Mapping) *esMapping.PropertyMapping {
+	res, _ := esMapping.NewPropertyMapping(mapping)
+	return res
 }
